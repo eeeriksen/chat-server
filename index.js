8d352bf4bf7c1e7e25455bb3ec9109df62695f48
Eriksen Lezama <eriksen.lezama@gmail.com>
2022-08-15 09:58:58 -0500

    Adding another origin domain to cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
   cors: {
-    origin: 'https://eriksend.com',
+    origin: ['https://eriksend.com', 'https://chat.eriksend.com'],
     methods: ['GET', 'POST']
   }
 })
